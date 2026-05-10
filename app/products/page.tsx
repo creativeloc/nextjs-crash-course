6ef9e6ed5ba8407f158a4a5a5d9174f63f5bea87
Lawrence Caulker <locaulker@gmail.com>
2026-05-10 15:19:19 -0400

    reviewed fetching data using client component, Note that this method is NOT SEO friendly

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,50 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
 interface Product {
   title: string
   id: number
 }
 
-const Products = async () => {
-  const data = await fetch("https://dummyjson.com/products")
-  const { products } = await data.json()
+// FETCHING DATA FROM SERVER COMPONENT
+// const Products = async () => {
+//   const data = await fetch("https://dummyjson.com/products")
+//   const { products } = await data.json()
+
+//   return (
+//     <div>
+//       {products.map((product: Product) => (
+//         <h3 key={product.id}>{product.title}</h3>
+//       ))}
+//     </div>
+//   )
+// }
+// export default Products
+
+// FETCHING DATA FROM CLIENT COMPONENT
+// First mark the component page at the top as "use client"
+const Products = () => {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products")
+        const { products: data } = await response.json()
+        setProducts(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchProducts()
+  }, [])
 
   return (
     <div>
-      {products.map((product: Product) => (
-        <h3 key={product.id}>{product.title}</h3>
-      ))}
+      {products.map((product: Product) => {
+        return <h1 key={product.id}>{product.title}</h1>
+      })}
     </div>
   )
 }
